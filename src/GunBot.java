e9eeb504b38cab95d4c44b2d3f8ca9f76e14b7a1
Darren <DarrenVawter>
2019-09-14 03:04:04 -0700

    first useable version 09/14/19

--- src/GunBot.java
+++ src/GunBot.java
@@ -28,7 +28,11 @@ public class GunBot {
 
     //grid vars
     private static final int X_OFFSET = 577;
-    private static final int Y_OFFSET = 347;
+    //windowed
+    private static final int Y_OFFSET = 368;
+    //fullscreen
+    //private static final int Y_OFFSET = 347;
+
     private static final int CELL_WIDTH = 32;
 
     //screen pixel vars
@@ -185,7 +189,7 @@ public class GunBot {
         //1->water
         //4->paper
         //5->powder
-        //2->not relevant
+        //3->not relevant
     private static byte getEntityAt(double gridX, double gridY){
         if(isColorAt(gridX,gridY,255,0,0)){
             return 5;
@@ -196,7 +200,7 @@ public class GunBot {
         }else if(isColorAt(gridX,gridY,0,0,255)){
             return 1;
         }else{
-            return 2;
+            return 3;
         }
     }
 
@@ -212,7 +216,7 @@ public class GunBot {
         if(isColorAt(gridX,gridY,255,255,255)) {
             //needs to be filled
             return 0;
-        }else if(isColorAt(gridX,gridY,0,0,0)) {
+        }else if(isColorAt(gridX,gridY,0,0,0) || isColorAt(gridX,gridY,255,0,0) || isColorAt(gridX,gridY,0,255,0)) {
             //needs to be cleaned
             return 1;
         }else if(isColorAt(gridX,gridY,128,128,128)) {
@@ -224,58 +228,115 @@ public class GunBot {
         }
     }
     private static void specifyGunStatus() {
-        //designate what gun needs (if it needs to be filled)
+
+        //designate what each gun needs (if front is empty)
+
+        //gun0
         if(gunStatus[0]==0) {
-            if(!isColorAt(2,3,0,255,0)) {
-                if(!isColorAt(3,3,255,0,0)) {
-                    //needs powder
-                    gunStatus[0]=5;
-                }else {
-                    //needs paper
+            //what is in the middle of the cannon?
+                //is it empty?
+            if(isColorAt(2,3,255,255,255)){
+                //is there powder in the back?
+                if(isColorAt(3,3,255,0,0)){
+                    //then needs paper
                     gunStatus[0]=4;
+                //if the back has something other than powder, needs to be washed
+                }else if(!isColorAt(3,3,255,255,255)){
+                    gunStatus[0]=1;
+                //otherwise the back needs powder
+                }else{
+                    gunStatus[0]=5;
                 }
+            //if the middle has something other than powder, needs to be washed
+            }else if(!isColorAt(2,3,0,255,0)){
+                gunStatus[0]=1;
             }
-            //needs shot (don't change status code)
+            //otherwsie the front needs a shot
+            else{
+                gunStatus[0]=0;
+            }
         }
+
+        //gun1
         if(gunStatus[1]==0) {
-            if(!isColorAt(10,3,0,255,0)) {
-                if(!isColorAt(9,3,255,0,0)) {
-                    //needs powder
-                    gunStatus[1]=5;
-                }else {
-                    //needs paper
+            //what is in the middle of the cannon?
+            //is it empty?
+            if(isColorAt(10,3,255,255,255)){
+                //is there powder in the back?
+                if(isColorAt(9,3,255,0,0)){
+                    //then needs paper
                     gunStatus[1]=4;
+                    //if the back has something other than powder, needs to be washed
+                }else if(!isColorAt(9,3,255,255,255)){
+                    gunStatus[1]=1;
+                    //otherwise the back needs powder
+                }else{
+                    gunStatus[1]=5;
                 }
+                //if the middle has something other than powder, needs to be washed
+            }else if(!isColorAt(10,3,0,255,0)){
+                gunStatus[1]=1;
             }
-            //needs shot (don't change status code)
+            //otherwsie the front needs a shot
+            else{
+                gunStatus[1]=0;
+            }
         }
+
+        //gun2
         if(gunStatus[2]==0) {
-            if(!isColorAt(2,9,0,255,0)) {
-                if(!isColorAt(3,9,255,0,0)) {
-                    //needs powder
-                    gunStatus[2]=5;
-                }else {
-                    //needs paper
+            //what is in the middle of the cannon?
+            //is it empty?
+            if(isColorAt(2,9,255,255,255)){
+                //is there powder in the back?
+                if(isColorAt(3,9,255,0,0)){
+                    //then needs paper
                     gunStatus[2]=4;
+                    //if the back has something other than powder, needs to be washed
+                }else if(!isColorAt(3,9,255,255,255)){
+                    gunStatus[2]=1;
+                    //otherwise the back needs powder
+                }else{
+                    gunStatus[2]=5;
                 }
+                //if the middle has something other than powder, needs to be washed
+            }else if(!isColorAt(2,9,0,255,0)){
+                gunStatus[2]=1;
             }
-            //needs shot (don't change status code)
+            //otherwsie the front needs a shot
+            else{
+                gunStatus[2]=0;
+            }
         }
+
+        //gun3
         if(gunStatus[3]==0) {
-            if(!isColorAt(10,9,0,255,0)) {
-                if(!isColorAt(9,9,255,0,0)) {
-                    //needs powder
-                    gunStatus[3]=5;
-                }else {
-                    //needs paper
+            //what is in the middle of the cannon?
+            //is it empty?
+            if(isColorAt(10,9,255,255,255)){
+                //is there powder in the back?
+                if(isColorAt(9,9,255,0,0)){
+                    //then needs paper
                     gunStatus[3]=4;
+                    //if the back has something other than powder, needs to be washed
+                }else if(!isColorAt(9,9,255,255,255)){
+                    gunStatus[3]=1;
+                    //otherwise the back needs powder
+                }else{
+                    gunStatus[3]=5;
                 }
+            //if the middle has something other than powder, needs to be washed
+            }else if(!isColorAt(10,9,0,255,0)){
+                gunStatus[3]=1;
             }
-            //needs shot (don't change status code)
+            //otherwsie the front needs a shot
+            else{
+                gunStatus[3]=0;
+            }
         }
 
         /*
-		for(int i=0;i<4;i++) {
+		for(int i=3;i<4;i++) {
 			switch(gunStatus[i]) {
 				case 0:
 					System.out.println("Gun "+i+" needs shot.");
@@ -297,7 +358,7 @@ public class GunBot {
 					break;
 			}
 		}
-		*/
+        */
     }
 
     //main method
@@ -355,12 +416,10 @@ public class GunBot {
 
             //refresh TL [0,5 (D)] and BR [12,7 (A)] loop spots if needed
             if(!arrowExists(0,5,D)){
-                System.out.println("refreshing TL");
                 createArrow(0,5,D);
                 recenterMouse();
             }
             if(!arrowExists(12,7,A)){
-                System.out.println("refreshing BR");
                 createArrow(12,7,A);
                 recenterMouse();
             }
@@ -368,14 +427,12 @@ public class GunBot {
             //check if position two above or two below barrel is (about to be) occupied with a piece
             if(isColorAt(6,4.55,255,0,0) || isColorAt(6,4.55,0,255,0) ||
                     isColorAt(6,4.55,0,0,255) ||isColorAt(6,4.55,255,0,255)) {
-                System.out.println("catching top");
                 createArrow(6,4,S);
                 createArrow(6,5,D);
                 recenterMouse();
             }
             if(isColorAt(6,7.45,255,0,0) || isColorAt(6,7.45,0,255,0) ||
                     isColorAt(6,7.45,0,0,255) ||isColorAt(6,7.45,255,0,255)) {
-                System.out.println("catching bot");
                 createArrow(6,8,W);
                 createArrow(6,7,A);
                 recenterMouse();
@@ -395,19 +452,83 @@ public class GunBot {
             specifyGunStatus();
 
             //get each gun what it needs
-            byte loop, entry;
+            byte loop, entryL, entryU;
 
             //gun 0
-            loop = getEntityAt(0,5.45);
-            entry = getEntityAt(0,3.4);
-
-            if(loop==gunStatus[0]){
-                createArrow(0,5,W);
-                recenterMouse();
+            if(gunStatus[0]!=2) {
+                loop = getEntityAt(0, 5.45);
+                entryU = getEntityAt(0, 2.65);
+                entryL = getEntityAt(0, 3.45);
+                if (loop == gunStatus[0]) {
+                    createArrow(0, 5, W);
+                    recenterMouse();
+                }
+                if (entryL == gunStatus[0] || entryU == gunStatus[0]) {
+                    createArrow(0, 3, D);
+                    recenterMouse();
+                } else if ((entryL != 3 || entryU != 3) && arrowExists(0,3,D)) {
+                    createArrow(0, 3, S);
+                    recenterMouse();
+                }
             }
-            if(entry==gunStatus[0]){
-                createArrow(0,3,D);
-                recenterMouse();
+            //gun1
+            else if(gunStatus[1]!=2){
+                loop = getEntityAt(11.55, 5);
+                entryU = getEntityAt(12, 2.65);
+                entryL = getEntityAt(12, 3.45);
+                if (loop == gunStatus[1]) {
+                    createArrow(12, 5, W);
+                    recenterMouse();
+                }else if(loop != 3 && arrowExists(12,5,W)){
+                    createArrow(12, 5, S);
+                    recenterMouse();
+                }
+
+                if (entryL == gunStatus[1] || entryU == gunStatus[1]) {
+                    createArrow(12, 3, A);
+                    recenterMouse();
+                } else if ((entryL != 3 || entryU != 3) && arrowExists(12,3,A)) {
+                    createArrow(12, 3, S);
+                    recenterMouse();
+                }
+            }
+
+            //gun 3
+            if(gunStatus[3]!=2) {
+                loop = getEntityAt(12, 6.55);
+                entryU = getEntityAt(12, 8.55);
+                entryL = getEntityAt(12, 8.55);
+                if (loop == gunStatus[3]) {
+                    createArrow(12, 7, S);
+                    recenterMouse();
+                }
+                if (entryL == gunStatus[3] || entryU == gunStatus[3]) {
+                    createArrow(12, 9, A);
+                    recenterMouse();
+                } else if ((entryL != 3 || entryU != 3) && arrowExists(0,3,A)) {
+                    createArrow(0, 3, W);
+                    recenterMouse();
+                }
+            }
+            //gun2
+            else if(gunStatus[2]!=2){
+                loop = getEntityAt(0.45, 7);
+                entryU = getEntityAt(0, 8.65);
+                entryL = getEntityAt(0, 9.45);
+                if (loop == gunStatus[2]) {
+                    createArrow(0, 7, S);
+                    recenterMouse();
+                }else if (loop != 3 && arrowExists(0,7,S)) {
+                    createArrow(0, 7, W);
+                    recenterMouse();
+                }
+                if (entryL == gunStatus[2] || entryU == gunStatus[2]) {
+                    createArrow(0, 9, D);
+                    recenterMouse();
+                } else if ((entryL != 3 || entryU != 3) && arrowExists(0,9,D)) {
+                    createArrow(0, 9, W);
+                    recenterMouse();
+                }
             }
 
         }
